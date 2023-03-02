92561fbc1ce5222a70b25e41f073eb102de45002
demka <kirillbazaroff@gmail.com>
2023-03-03 00:42:24 +0300

    404 handler

--- flsite.py
+++ flsite.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
@@ -14,6 +14,11 @@ def about():
 @app.route("/blog")
 def blog():
     return render_template('base.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
